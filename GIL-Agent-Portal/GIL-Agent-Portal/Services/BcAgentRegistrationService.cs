e8604723c8d62c1eebd93fb91b96ae1203934bfe
Bhavisha <bhavisha@prashisolution.com>
2025-07-29 18:18:17 +0530

    checksum testing

--- GIL-Agent-Portal/GIL-Agent-Portal/Services/BcAgentRegistrationService.cs
+++ GIL-Agent-Portal/GIL-Agent-Portal/Services/BcAgentRegistrationService.cs
@@ -41,13 +41,29 @@ namespace GIL_Agent_Portal.Services
                 model.token = token;
                 model.bcid = bcid;
 
+                //actual checksum......
+                //string checksum = $"{model.channelid}{model.appid}{model.partnerid}{model.bcid}{model.bcagentid}{model.bcagentname}{model.middlename}{model.lastname}{model.companyname}" +
+                //                  $"{model.address}{model.statename}{model.cityname}{model.district}{model.area}{model.pincode}{model.mobilenumber}{model.telephone}{model.alternatenumber}{model.emailid}{model.dob}" +
+                //                  $"{model.shopaddress}{model.shopstate}{model.shopcity}{model.shopdistrict}{model.shoparea}{model.shoppincode}{model.pancard}{model.bcagentform}" +
+                //                  $"{model.productdetails?.dmt}{model.productdetails?.aeps}{model.productdetails?.cardpin}{model.productdetails?.accountopen}" +
+                //                  $"{model.terminaldetails?.tposserialno}{model.terminaldetails?.taddress}{model.terminaldetails?.taddress1}{model.terminaldetails?.tpincode}" +
+                //                  $"{model.terminaldetails?.tcity}{model.terminaldetails?.tstate}{model.terminaldetails?.temail}{model.agenttype}{model.agentbcid}{model.token}";
 
-                string checksum = $"{model.channelid}{model.appid}{model.partnerid}{model.bcid}{model.bcagentid}{model.bcagentname}{model.middlename}{model.lastname}{model.companyname}" +
-                                  $"{model.address}{model.statename}{model.cityname}{model.district}{model.area}{model.pincode}{model.mobilenumber}{model.telephone}{model.alternatenumber}{model.emailid}{model.dob}" +
-                                  $"{model.shopaddress}{model.shopstate}{model.shopcity}{model.shopdistrict}{model.shoparea}{model.shoppincode}{model.pancard}{model.bcagentform}" +
-                                  $"{model.productdetails?.dmt}{model.productdetails?.aeps}{model.productdetails?.cardpin}{model.productdetails?.accountopen}" +
-                                  $"{model.terminaldetails?.tposserialno}{model.terminaldetails?.taddress}{model.terminaldetails?.taddress1}{model.terminaldetails?.tpincode}" +
-                                  $"{model.terminaldetails?.tcity}{model.terminaldetails?.tstate}{model.terminaldetails?.temail}{model.agenttype}{model.agentbcid}{model.token}";
+                //string checksum = $"{model.channelid}{model.appid}{model.partnerid}{model.bcid}{model.bcagentid}{model.bcagentname}{model.middlename}{model.lastname}{model.companyname}" +
+                //         $"{model.address}{model.statename}{model.cityname}{model.district}{model.area}{model.pincode}{model.mobilenumber}{model.telephone}{model.alternatenumber}{model.emailid}{model.dob}" +
+                //         $"{model.shopaddress}{model.shopstate}{model.shopcity}{model.shopdistrict}{model.shoparea}{model.shoppincode}{model.pancard}{model.bcagentform}" +
+                //         $"{model.productdetails?.dmt ?? ""}{model.productdetails?.aeps ?? ""}{model.productdetails?.cardpin ?? ""}{model.productdetails?.accountopen ?? ""}" +
+                //         $"{model.terminaldetails?.tposserialno ?? ""}{model.terminaldetails?.taddress ?? ""}{model.terminaldetails?.taddress1 ?? ""}{model.terminaldetails?.tpincode ?? ""}" +
+                //         $"{model.terminaldetails?.tcity ?? ""}{model.terminaldetails?.tstate ?? ""}{model.terminaldetails?.temail ?? ""}{model.agenttype}{model.agentbcid}{model.token}";
+
+                string checksum = $"{model.channelid?.Trim()}{model.appid?.Trim()}{model.partnerid?.Trim()}{model.bcid?.Trim()}{model.bcagentid?.Trim()}{model.bcagentname?.Trim()}{model.middlename?.Trim()}{model.lastname?.Trim()}{model.companyname?.Trim()}" +
+                  $"{model.address?.Trim()}{model.statename?.Trim()}{model.cityname?.Trim()}{model.district?.Trim()}{model.area?.Trim()}{model.pincode?.Trim()}{model.mobilenumber?.Trim()}{model.telephone?.Trim()}{model.alternatenumber?.Trim()}{model.emailid?.Trim()}{model.dob?.Trim()}" +
+                  $"{model.shopaddress?.Trim()}{model.shopstate?.Trim()}{model.shopcity?.Trim()}{model.shopdistrict?.Trim()}{model.shoparea?.Trim()}{model.shoppincode?.Trim()}{model.pancard?.Trim()}{model.bcagentform?.Trim()}" +
+                  $"{model.productdetails?.dmt?.Trim() ?? ""}{model.productdetails?.aeps?.Trim() ?? ""}{model.productdetails?.cardpin?.Trim() ?? ""}{model.productdetails?.accountopen?.Trim() ?? ""}" +
+                  $"{model.terminaldetails?.tposserialno?.Trim() ?? ""}{model.terminaldetails?.taddress?.Trim() ?? ""}{model.terminaldetails?.taddress1?.Trim() ?? ""}{model.terminaldetails?.tpincode?.Trim() ?? ""}" +
+                  $"{model.terminaldetails?.tcity?.Trim() ?? ""}{model.terminaldetails?.tstate?.Trim() ?? ""}{model.terminaldetails?.temail?.Trim() ?? ""}{model.agenttype?.Trim()}{model.agentbcid?.Trim()}{model.token?.Trim()}";
+
+
 
                 model.signcs = NsdlSignCsHelper.GenerateSignCs(secretKey, checksum);
 
